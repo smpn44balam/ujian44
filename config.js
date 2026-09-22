d136fc61f8f587b54b790cea4ecd0aa719b84853
SMP N 44 Bandar Lampung <mjakagunawan1@gmail.com>
2026-09-22 09:22:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const NEXORA_CONFIG = {
   // window.NEXORA_CONFIG.scriptUrl. Sebelumnya key ini bernama "monitoringUrl"
   // sehingga tidak pernah terbaca dan semua log pelanggaran/heartbeat GAGAL TERKIRIM
   // secara diam-diam (silent fail). Jangan ganti nama key ini lagi.
-  scriptUrl: "https://script.google.com/macros/s/AKfycbx9JYuBxjMj_iieOsTtiw1DEto8tyWg0EcdGH6SyCW99lxpRVA8rpVchJnkyYdjyZC8QQ/exec",
+  scriptUrl: "https://script.google.com/macros/s/AKfycbw8s20m5OxZEd-XdwL7synSGxsVqepfPW72RrotyASuEVciv7vGXMHqCJtjVEGJbkUL1A/exec",
   // Alias lama, dipertahankan agar kode lama yang mungkin masih memanggil
   // monitoringUrl tidak langsung pecah.
   get monitoringUrl() { return this.scriptUrl; },
